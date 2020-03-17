fc794bbf3dc330fb6dcc75d1ec48c5684131e685
doberman257 <51162801+doberman257@users.noreply.github.com>
2020-03-17 20:42:31 +0100

    upload files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+        artistInput: '',
+        songInput: '',
+        lyrics: ''
+    }
+  }
+
+  lyricsButton = () => {
+    this.componentDidMount()
+  }
+
+
+  componentDidMount(){
+    const {artistInput, songInput} = this.state
+    fetch(`https://api.lyrics.ovh/v1/${artistInput}/${songInput}`)
+    .then(res => res.json())
+    .then(data => this.setState({lyrics: data}))
+  }
+
+  render(){
+      console.log(this.state)
+      return(
+          <div className='container App-header'>
+            <h1 className='mt-5'>Find Your Favorite Lyrics <i className="fas fa-music"></i></h1>
+            <div className='mt-1 mb-1 d-sm-flex flex-sm-column justify-content-sm-center'>
+              <input type='text' className='m-1 ' placeholder=" Artist" onChange={(e) => this.setState({artistInput: e.target.value})} />
+              <input type='text' className='m-1' placeholder=" Song" onChange={(e) => this.setState({songInput: e.target.value})} />
+            </div> 
+              <button type='submit' className='btn btn-primary mb-5 mt-2' onClick={this.lyricsButton}>Search</button>
+              <div className='mb-5 lyrics-font container'>
+                {this.state.lyrics.lyrics}
+              </div>       
+          </div>
+      )
+  }
 }
 
 export default App;
